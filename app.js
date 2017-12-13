18b19d5c9cbc6d7fe9e5e30c59cf5ab3b26300a8
Karen Geerts <karen.geerts@gmail.com>
2017-12-13 14:32:57 +1100

    before removing HTML niceties

--- app.js
+++ app.js
@@ -1,23 +1,46 @@
-/*TO DO: new approach where page scrolls down when something is selected*/
-//lines 2-20 related to look and feel, no game functionality
-/*var initialView = document.querySelector('#openSelectSettingsView');
+var initialView = document.querySelector('#openSelectSettingsView');
 var selectSettings = document.querySelector('.selectSettingsView');
+var gamePlayView = document.querySelector('.gamePlayView');
 var letsPlayBtn = document.querySelector('.playBtn');
+var boardCanvas = document.querySelector('.grid');
 
 //this function hides the opening image and shows the Select Settings options
 var openSelectSettings = function(){
-  initialView.classList.add('hidden');
-  selectSettings.classList.add('visible');
+  selectSettings.scrollIntoView();
 };
 
 //this function hides the Select Settings options and shows the gameboard
 var startGame = function(){
-  selectSettings.classList.add('hidden');
-  console.log("Lets go.")
-}
+  gamePlayView.scrollIntoView();
+  };
 
-initialView.addEventListener('click',openSelectSettings);
-*/
+//this function is based on https://github.com/LearnTeachCode/Battleship-JavaScript/blob/gh-pages/battleship.js
+//it builds the board on the screen and creates unique id's for each small square (div)
+var showBoard = function (columns = 3,rows = 3){
+  // set grid rows and columns and the size of each square
+  var squareSize = 50;
+
+// make the grid columns and rows
+  for (i = 0; i < columns; i++) {
+	   for (j = 0; j < rows; j++) {
+		// create a new div HTML element for each grid square and make it the right size
+		  var square = document.createElement("div");
+		  boardCanvas.appendChild(square);
+      square.classList.add("sq1");
+      // give each div element a unique id based on its row and column, like "s00"
+		  square.id = 's' + j + i;
+
+		// set each grid square's coordinates: multiples of the current row or column number
+		  var topPosition = j * squareSize;
+		  var leftPosition = i * squareSize;
+
+		// use CSS absolute positioning to place each grid square on the page
+		  square.style.top = topPosition + 'px';
+		  square.style.left = leftPosition + 'px';
+	  }
+  }
+};
+
 
 //this function returns an array of arrays
 var buildGrid = function(columns,rows){
@@ -31,7 +54,7 @@ var buildGrid = function(columns,rows){
   return grid;
 };
 
-//this function creates all horizontal strings in an array of arrays and returns these strings as elements of an arrays
+//this function creates all horizontal strings in an array of arrays and returns these strings as elements of an array
 var createHorizontalStrings = function(array,rows){
   var horizontalStringArray = [];
   for (var i = 0; i < rows; i++ ){
@@ -41,6 +64,7 @@ var createHorizontalStrings = function(array,rows){
   return horizontalStringArray;
 };
 
+//this function creates all vertical strings in an array of arrays and returns these strings as elements of an array
 var createVerticalStrings = function(array,columns,rows){
   var verticalLineArray = [];
   var verticalStringArray = [];
@@ -55,8 +79,10 @@ var createVerticalStrings = function(array,columns,rows){
   return verticalStringArray;
 };
 
+//this function creates all diagonal strings in an array of arrays and returns these strings as elements of an array
 //copied from https://stackoverflow.com/questions/35917734/how-do-i-traverse-an-array-diagonally-in-javascript
 //updated variable names to increase readability
+//bottomToTop takes Boolean value to determine direction of diagonal lines
 var createDiagonalStrings = function(array, columns, rows, bottomToTop) {
   var numOfHorLines = columns+rows-1;
   var temp;
@@ -74,28 +100,35 @@ var createDiagonalStrings = function(array, columns, rows, bottomToTop) {
         }
     }
   return returnArray;
-}
+};
 
-
-
-//code below superceded by function
-/*
-var createHorizontalStringsBottomLeftToTopRight = function(array,columns,rows){
-  var numOfHorLines = columns + rows - 1;
-  var lineArray = [];
-  var horLineBtmLeftToTopRightArray = [];
-  for (var i = 0; i < numOfHorLines; i++) { //this number of strings need to be created
-    for (var j = 0; j < rows; j++){
-      var element = array[j][i];
-      lineArray.push(element); //add the element to the lineArray
-      if ((j))//check if you can go one row up
-
-
-    }
-
+var checkForWinningSequence = function(array, winningSequence){
+  var checkValueArray = [];
+  for (var i = 0; i < array.length; i++){
+    checkValueArray.push(array[i].includes(winningSequence));
   }
+  var checkValue = checkValueArray.includes(true);
+  return checkValue; //boolean to indicate whether or not there is a winning sequence in the array
+};
 
-};*/
+//this function calls all of the string creation functions and checks them for the winning sequence
+//if one the strings contains the winning sequence, the function returns a boolean true
+var checkGrid = function(grid, columns, rows, winningSequence){
+  var horizontalWinner = checkForWinningSequence(createHorizontalStrings(grid,rows), winningSequence);
+  var verticalWinner = checkForWinningSequence(createVerticalStrings(grid,columns,rows), winningSequence);
+  var diagonalWinner1 = checkForWinningSequence(createDiagonalStrings(grid,columns,rows, true), winningSequence);
+  var diagonalWinner2 = checkForWinningSequence(createDiagonalStrings(grid,columns,rows, false), winningSequence);
+  var checkResultArray = [horizontalWinner, verticalWinner, diagonalWinner1, diagonalWinner2];
+  var checkResult = checkResultArray.includes(true);
+  return checkResult;
+};
+
+
+//adding EventListeners
+
+initialView.addEventListener('click',openSelectSettings);
+letsPlayBtn.addEventListener('click',startGame);
+
 
 
 //lines below are used to test functionality and should not go into production
